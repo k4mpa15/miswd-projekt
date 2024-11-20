7a8254138fece3b48f0237f25ce9f1bc543a6a80
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-11-20 16:48:07 +0100

    drobny fix

--- generator/gui.py
+++ generator/gui.py
@@ -8,7 +8,7 @@ import re
 class GUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("Konwerter Równań")
+        self.root.title("Generator Równań")
 
         # Ustawienia ogólne rozmiaru
         self.root.geometry("600x700")  # Ustawienie większego rozmiaru okna
